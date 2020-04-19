2cbfbcfe44b972806b6a7640deb6fe1d0f26572e
Delfio <delfio_eu@hotmail.com>
2020-04-18 21:51:02 -0400

    desafio completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,98 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  try {
+
+    return response.json(repositories);
+
+  } catch(err){
+    return response.json(err.message);
+  }
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  try {
+    const { title, url, techs,  } = request.body;
+    const repository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+
+    repositories.push(repository);
+
+    return response.json(repository)
+  } catch(er){
+    return response.json(er.message);
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  try {
+    const {id} = request.params;
+    const {title, url, techs } = request.body;
+    const indexProjeto = repositories.findIndex(prjs => prjs.id === id);
+    if(indexProjeto < 0){
+      return response.status(400).json({error: 'Project not found'});
+    }
+
+    const projectAntigo = repositories[indexProjeto];
+    const newProject = repositories[indexProjeto]= {
+      id,
+      title,
+      url,
+      techs,
+      likes: projectAntigo.likes
+    }
+
+    return response.json(newProject);
+  } catch(err) {
+    return response.json(err.message);
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  try{
+    const {id} = request.params;
+    const projectIndex = repositories.findIndex(proj => proj.id === id);
+
+    if(projectIndex < 0){
+      return response.status(400).json({error: "Repository not found"})
+    }
+    repositories.splice(projectIndex, 1);
+
+    return response.status(204).json({ok: true})
+  } catch (err) {
+    return response.status(400).json(err.message)
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  try {
+    const {id} = request.params;
+    if(id < 0) {
+      return response.status(400).json({error: 'Like menor q 0?'});
+    }
+
+    const indexProjeto = repositories.findIndex(prjs => prjs.id === id);
+    if(indexProjeto < 0){
+      return response.status(400).json({error: 'Project not found'});
+    }
+
+    const projectAntigo = repositories[indexProjeto];
+    const projectComLike = repositories[indexProjeto]= {
+      id: projectAntigo.id,
+      title: projectAntigo.title,
+      url: projectAntigo.url,
+      tech: projectAntigo.tech,
+      likes: projectAntigo.likes +1
+    }
+    return response.json(projectComLike);
+  } catch(err) {
+    return response.status(400).json(err.message);
+  }
 });
 
 module.exports = app;
